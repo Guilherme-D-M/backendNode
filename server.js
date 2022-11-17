a7578af5ba51c4fba2c6258daa85f0cebf9c310b
Guilherme-D-M <guidal1050@outlook.com>
2022-11-17 15:51:00 -0300

    Passa o conteudo do app.js para o servidor

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 //Importando para o projeto o Http
 const http = require('http');
 
+//Importa o app.js para o server
+const app = require('./app');
+
 //Variavel para armazenar a porta | Caso a porta não estiver preenchida, pega a 3000
 const port = process.env.PORT || 3000;
 
 //cria o servidor
-const server = http.createServer();
+const server = http.createServer(app);
 //Insere a porta no servidor
 server.listen(port);
 
