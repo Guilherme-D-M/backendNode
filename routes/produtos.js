c71ea1962a5c321165a69cfbdbb5677413f9cfbc
Guilherme-D-M <guidal1050@outlook.com>
2022-11-17 17:04:11 -0300

    Criado rotas de patch e delete

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+// Retorna todos os produtos
 router.get('/', (req, res, next) => {
     res.status(200).send({
         mensagem: 'Usando o get dentro da rota de produtos'
     });
 });
 
+// Insere um produto
 router.post('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Usando o post dentro da rota de produtos'
     });
 });
 
+// Retorna os dados de um produto
 router.get('/:id_produto', (req, res, next) => {
     //Pega o parametro do id que foi passado na url
     const id = req.params.id_produto;
@@ -28,6 +31,18 @@ router.get('/:id_produto', (req, res, next) => {
             id: id
         });
     }
+});
+
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o PATCH dentro da rota de produtos'
+    });
+});
+
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o DELETE dentro da rota de produtos'
+    });
 });
 
 
